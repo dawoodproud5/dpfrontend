98536dc6d53a6d4b4752185d482de7929db81153
Engr. Raheel <engr.mraheeltariq@gmail.com>
2023-07-30 04:13:24 +0500

    herosection testing

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      
       <Head />
       <link rel="icon" href="/favicon.ico" />
-     
-
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body>
         <Main />
         <NextScript />
