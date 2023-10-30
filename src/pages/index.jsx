bdde8ea7362e1af4581bda2b01e4504f3d45142e
TeamProgmaticAI <147185548+progmaticAI@users.noreply.github.com>
2023-10-31 03:42:24 +0500

    meta tag udpate for site varification

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,32 @@
-
 import Layout from "../../components/Layout/Layout";
 import Head from "next/head";
 
-
 const Home = () => {
   return (
-
     <>
       <Head>
-        <title>Dawood Proud: YouTube Agency for all YouTube Growth Services</title>
-        <meta name="description" content="Leading YouTube Channel Growth Agency that offers comprehensive Content Strategy and Best YouTube Growth Services to dominate the platform." />
-        <meta name="keywords" content="YouTube Agency, YouTube Growth Agency, YouTube Channel Growth Agency, Dawood Proud, DawoodProud" />
+        <title>
+          Dawood Proud: YouTube Agency for all YouTube Growth Services
+        </title>
+        <meta
+          name="google-site-verification"
+          content="google5bdab0d566274652.html"
+        />
+        <meta
+          name="description"
+          content="Leading YouTube Channel Growth Agency that offers comprehensive Content Strategy and Best YouTube Growth Services to dominate the platform."
+        />
+        <meta
+          name="keywords"
+          content="YouTube Agency, YouTube Growth Agency, YouTube Channel Growth Agency, Dawood Proud, DawoodProud"
+        />
         <meta name="author" content="Dawood Proud" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="canonical" href="https://www.dawoodproud.com/" />
       </Head>
       <Layout />
     </>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
