eb5cc28ed31ca5cabd0940051c943cf23b6b50d1
TeamProgmaticAI <147185548+progmaticAI@users.noreply.github.com>
2023-10-31 03:37:16 +0500

    site owner ship

---
+++ src/pages/google5bdab0d566274652.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Verification() {
+  return (
+    <Head>
+      <meta
+        name="google-site-verification"
+        content="google5bdab0d566274652.html"
+      />
+    </Head>
+  );
+}
--- src/pages/verification.js
+++
@@ -1,3 +0,0 @@
-export default function Verification() {
-  return <>google-site-verification: google5bdab0d566274652.html</>;
-}
